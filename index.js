9ab86a75deb6289cd7347a28bb35ccb1a4dbf7cd
ann gamal <anngamal14@gmail.com>
2023-07-16 17:45:04 -0400

    completed labs

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(namesArray){
+    let thankYouCards = []
+    for(let i = 0; i < namesArray.length; i++){
+        thankYouCards.push(`Thank you, ${namesArray[i]}, for the wonderful surprise gift!`)
+    }
+return thankYouCards;
+}
+function countDown (number){
+    for(let i = 10; i >= 0; i--){
+      console.log(i)  
+    }
+}
+ 
